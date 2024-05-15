484151292eb5c12f8c15e06e903f1c982d00e8ed
DannyAkintunde <doakintunde@outlook.com>
2024-05-15 03:23:56 +0100

    database

--- kjcnums/settings.py
+++ kjcnums/settings.py
@@ -82,12 +82,7 @@ WSGI_APPLICATION = 'kjcnums.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
