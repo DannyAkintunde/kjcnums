fd2f6989d026bb33f93c9c909d79d78fff488b3d
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-07-16 00:53:04 +0100

    Update settings.py

--- kjcnums/settings.py
+++ kjcnums/settings.py
@@ -143,6 +143,12 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
