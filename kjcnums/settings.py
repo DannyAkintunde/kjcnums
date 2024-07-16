554fb1be85b77b7e838955d728ae5f33b5923e89
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-07-16 01:06:27 +0100

    Update settings.py

--- kjcnums/settings.py
+++ kjcnums/settings.py
@@ -135,7 +135,7 @@ LOGOUT_REDIRECT_URL ='/login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
